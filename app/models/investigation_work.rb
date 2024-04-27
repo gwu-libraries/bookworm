bdfe7e64b099a5d7bac84394e5e1d4781e5bac78
Alex Boyd <alexanderleeboyd@gmail.com>
2024-04-27 14:52:56 -0400

    remove investigation_work model

--- app/models/investigation_work.rb
+++
@@ -1,7 +0,0 @@
-class InvestigationWork < ApplicationRecord
-  validates :investigation_id, presence: true
-  validates :work_id, presence: true
-
-  belongs_to :investigation
-  belongs_to :work
-end
