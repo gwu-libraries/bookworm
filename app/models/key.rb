166aa8db7f848eba882ea5782a6f5c134fbf7ad6
Alex Boyd <alexanderleeboyd@gmail.com>
2024-04-27 18:04:03 -0400

    Add user_id key validation

--- app/models/key.rb
+++ app/models/key.rb
@@ -1,6 +1,7 @@
 class Key < ApplicationRecord
   validates :site, presence: true
   validates :value, presence: true
+  validates :user_id, presence: true
 
   belongs_to :user
 end
--- spec/models/key_spec.rb
+++ spec/models/key_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Key, type: :model do
   describe 'validations' do
     it { should validate_presence_of :site }
     it { should validate_presence_of :value }
+    it { should validate_presence_of :user_id }
   end
 
   describe 'relationships' do
