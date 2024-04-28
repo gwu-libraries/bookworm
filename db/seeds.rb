59fdb3f1602b83264190bdab3679e9657c67bd23
Alex Boyd <alexanderleeboyd@gmail.com>
2024-04-27 22:41:59 -0400

    Seed start

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+user = FactoryBot.create(:user, email: "admin@example.com")
+key_1 = FactoryBot.create(:key, user_id: user.id, site: "scholarzone")
+
+investigation_1 = FactoryBot.create(:investigation, user_id: user.id, name: "Investigation 1")
+# root work
+root_work = FactoryBot.create(:work, investigation_id: investigation_1.id, title: "Root Work")
+# citations
+citations = FactoryBot.create_list(:work, 20, investigation_id: investigation_1.id)
+citations.map{ |citation| Connection.create(reference_id: root_work.id, citation_id: citation.id) }
+# references
+references = FactoryBot.create_list(:work, 20, investigation_id: investigation_1.id)
+references.map{ |reference| Connection.create(reference_id: reference.id, citation_id: root_work.id) }
