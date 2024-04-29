043e40426045c755f0f86c0eb6244f50535185d1
Alex Boyd <alexanderleeboyd@gmail.com>
2024-04-28 21:33:00 -0400

    Refactor work request

--- spec/requests/api/v1/works_request_spec.rb
+++ spec/requests/api/v1/works_request_spec.rb
@@ -2,36 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'works API' do
 
+  before :each do
+    @user = FactoryBot.create(:user)
+    @investigation = FactoryBot.create(:investigation, user_id: @user.id)
+
+    @work_1 = FactoryBot.create(:work, investigation_id: @investigation.id)
+    @work_2 = FactoryBot.create(:work, investigation_id: @investigation.id)
+    @work_3 = FactoryBot.create(:work, investigation_id: @investigation.id)
+  end
+
   it 'can return one work by ID' do
-    user = FactoryBot.create(:user)
-    investigation = FactoryBot.create(:investigation, user_id: user.id)
-
-    work = FactoryBot.create(:work, investigation_id: investigation.id)
-
-    get api_v1_work_path(work.id)
+    get api_v1_work_path(@work_1.id)
 
     expect(response).to be_successful
 
     work_response = JSON.parse(response.body, symbolize_names: true)[:data].first
 
     expect(work_response).to have_key(:id)
-    expect(work_response[:id]).to eq(work.id)
+    expect(work_response[:id]).to eq(@work_1.id)
     expect(work_response).to have_key(:investigation_id)
-    expect(work_response[:investigation_id]).to eq(investigation.id)
+    expect(work_response[:investigation_id]).to eq(@investigation.id)
     expect(work_response).to have_key(:doi)
-    expect(work_response[:doi]).to eq(work.doi)
+    expect(work_response[:doi]).to eq(@work_1.doi)
     expect(work_response).to have_key(:title)
-    expect(work_response[:title]).to eq(work.title)
+    expect(work_response[:title]).to eq(@work_1.title)
   end
 
   it 'can return a list of all works' do
-    user = FactoryBot.create(:user)
-    investigation = FactoryBot.create(:investigation, user_id: user.id)
-      work_1 = FactoryBot.create(:work, investigation_id: investigation.id)
-      work_2 = FactoryBot.create(:work, investigation_id: investigation.id)
-      work_3 = FactoryBot.create(:work, investigation_id: investigation.id)
-
     get api_v1_works_path
+
     works_response = JSON.parse(response.body, symbolize_names: true)[:data]
     
     expect(response).to be_successful
@@ -44,13 +43,10 @@ RSpec.describe 'works API' do
   end
 
   it 'can create a new work' do
-    user = FactoryBot.create(:user)
-    investigation = FactoryBot.create(:investigation, user_id: user.id)
-
     headers = {"CONTENT_TYPE" => "application/json"}
 
     work_params = {
-      investigation_id: "#{investigation.id}",
+      investigation_id: "#{@investigation.id}",
       title: "A Super Cool Work",
       doi: "10.1111/2222222"
     }
@@ -63,15 +59,12 @@ RSpec.describe 'works API' do
     new_work = Work.last
     
     expect(response_data[:id]).to eq(new_work.id)
-    expect(response_data[:investigation_id]).to eq(investigation.id)
+    expect(response_data[:investigation_id]).to eq(@investigation.id)
     expect(response_data[:title]).to eq(new_work.title)
     expect(response_data[:doi]).to eq(new_work.doi)
   end
 
   it 'does not create a new work if investigation_id is blank' do
-    user = FactoryBot.create(:user)
-    investigation = FactoryBot.create(:investigation, user_id: user.id)
-
     headers = {"CONTENT_TYPE" => "application/json"}
 
     work_params = {
@@ -85,13 +78,10 @@ RSpec.describe 'works API' do
   end
 
   it 'does not create a new work if title is blank' do
-    user = FactoryBot.create(:user)
-    investigation = FactoryBot.create(:investigation, user_id: user.id)
-
     headers = {"CONTENT_TYPE" => "application/json"}
 
     work_params = {
-      investigation_id: "#{investigation.id}",
+      investigation_id: "#{@investigation.id}",
       doi: "10.1111/2222222"
     }
 
@@ -101,13 +91,10 @@ RSpec.describe 'works API' do
   end
 
   it 'does not create a new work if doi is blank' do
-    user = FactoryBot.create(:user)
-    investigation = FactoryBot.create(:investigation, user_id: user.id)
-
     headers = {"CONTENT_TYPE" => "application/json"}
 
     work_params = {
-      investigation_id: "#{investigation.id}",
+      investigation_id: "#{@investigation.id}",
       title: "A Super Cool New Work"
     }
 
@@ -117,43 +104,39 @@ RSpec.describe 'works API' do
   end
 
   it 'can delete a work' do
-    user = FactoryBot.create(:user)
-    investigation = FactoryBot.create(:investigation, user_id: user.id)
-    work = FactoryBot.create(:work, investigation_id: investigation.id)
-
     headers = {"CONTENT_TYPE" => "application/json"}
 
-    delete api_v1_work_path(work.id), headers: headers
+    delete api_v1_work_path(@work_1.id), headers: headers
 
     expect(response).to be_successful
     expect(response.status).to eq(204)
-    expect(Work.count).to eq(0)
+    expect(Work.count).to eq(2)
   end
 
   it 'can update a work' do
-    user = FactoryBot.create(:user)
-    investigation = FactoryBot.create(:investigation, user_id: user.id)
-    work = FactoryBot.create(:work, title: "The Original Title", investigation_id: investigation.id)
-
     headers = {"CONTENT_TYPE" => "application/json"}
 
     work_params = {
       title: "A Cool New Title"
     }
 
-    patch api_v1_work_path(work.id), headers: headers, params: JSON.generate(work_params)
+    patch api_v1_work_path(@work_1.id), headers: headers, params: JSON.generate(work_params)
 
     work_response = JSON.parse(response.body, symbolize_names: true)[:data].first
 
     expect(response).to be_successful
     expect(work_response).to have_key(:id)
-    expect(work_response[:id]).to eq(work.id)
+    expect(work_response[:id]).to eq(@work_1.id)
     expect(work_response).to have_key(:investigation_id)
-    expect(work_response[:investigation_id]).to eq(investigation.id)
+    expect(work_response[:investigation_id]).to eq(@investigation.id)
     expect(work_response).to have_key(:doi)
-    expect(work_response[:doi]).to eq(work.doi)
+    expect(work_response[:doi]).to eq(@work_1.doi)
     expect(work_response).to have_key(:title)
     expect(work_response[:title]).to eq("A Cool New Title")
+  end
+
+  xit 'can add a citation to a work' do
+    
   end
 
 end
