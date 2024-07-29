f699fd987f3e896f9b64833fdf495a69939d44fe
Alex Boyd <alexanderleeboyd@gmail.com>
2024-07-29 19:46:01 -0400

    openalex service test coverage

--- app/poros/crossref_citation.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class CrossrefCitation
-  def initialize()
-  end
-end
--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 class BaseService
-  def self.semantic_scholar_connection
-    Faraday.new(url: 'https://api.semanticscholar.org') do |faraday|
-      faraday.headers['x-api-key'] = ENV['semantic_scholar_key']
-    end
-  end
-
-  def self.crossref_connection
-    Faraday.new(url: 'https://api.crossref.org') do |faraday|
-      faraday.headers['mailto'] = ENV['crossref_mailto']
-    end
-  end
-
   def self.openalex_connection
     Faraday.new(url: 'https://api.openalex.org') do |faraday|
       faraday.headers['mailto'] = ENV['openalex_mailto']
