4599e32577dd07bbf17f89e1d947b8f6aa3fbf54
Alex Boyd <alexanderleeboyd@gmail.com>
2024-07-29 20:19:55 -0400

    Add openalexfacade tests

--- spec/facades/openalex_facade_spec.rb
+++ spec/facades/openalex_facade_spec.rb
@@ -30,4 +30,47 @@ RSpec.describe OpenalexFacade do
     expect(work.topics).to be_a(Array)
     expect(work.type).to be_a(String)
   end
+
+  it 'returns an array of OpenAlex reference objects', :vcr do
+    openalex_id = 'W2785752813'
+
+    references = OpenalexFacade.get_paper_references(openalex_id)
+
+    expect(references).to be_a(Array)
+    expect(references.first).to be_a(OpenalexReference)
+  end
+
+  it 'returns an array of OpenAlex citation objects', :vcr do
+    openalex_id = 'W2785752813'
+
+    references = OpenalexFacade.get_paper_citations(openalex_id)
+
+    expect(references).to be_a(Array)
+    expect(references.first).to be_a(OpenalexCitation)
+  end
+
+  it 'returns an OpenAlex author object', :vcr do
+    openalex_id = 'A5023888391'
+
+    author = OpenalexFacade.get_author_details(openalex_id)
+
+    expect(author).to be_a(OpenalexAuthor)
+
+    expect(author.affiliations).to be_a(Array)
+    expect(author.cited_by_count).to be_a(Integer)
+    expect(author.display_name).to be_a(String)
+    expect(author.openalex_id).to be_a(String)
+    expect(author.orcid).to be_a(String)
+    expect(author.works_count).to be_a(Integer)
+  end
+
+  it 'returns an array of an authors works', :vcr do
+    openalex_id = 'A5023888391'
+
+    works = OpenalexFacade.get_author_works(openalex_id)
+
+    expect(works).to be_a(Array)
+
+    expect(works.first).to be_a(OpenalexWork)
+  end
 end
