756f2179853cf55945ff4c01db67dd52bd6dd7a6
Alex Boyd <alexanderleeboyd@gmail.com>
2024-08-16 09:39:07 -0400

    cont

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -6,6 +6,10 @@ module Types
     field :name, String
     field :orcid, String
     field :openalex_id, String
+    field :h_index, Integer
+    field :i10_index, Integer
+    field :cited_by_count, Integer
+    field :works_count, Integer
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
