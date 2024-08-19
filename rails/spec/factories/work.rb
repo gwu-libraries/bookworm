36863964a76bf8d0b3a12b417d2da09cab50c23b
Alex Boyd <alexanderleeboyd@gmail.com>
2024-08-19 18:31:47 -0400

    Initial commit in dockerized repo

--- rails/spec/factories/work.rb
+++ rails/spec/factories/work.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :work do
-    doi { Faker::Archival.doi }
+    doi { (0...8).map { (65 + rand(26)).chr }.join } ##this is just a random string
     title { Faker::Book.title }
     language { 'en' }
     publication_year { 2000 }
