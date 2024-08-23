6eb3f0b960bc4edd8ddf96e34f13f74e25516b49
Alex Boyd <alexanderleeboyd@gmail.com>
2024-08-23 17:31:39 -0400

    Add updating node positions every 10 seconds

--- react/src/components/graph/nodes/WorkNode.tsx
+++ react/src/components/graph/nodes/WorkNode.tsx
@@ -7,6 +7,7 @@ import { USE_INVESTIGATION_GRAPH } from "../../../hooks/useInvestigationGraph";
 import WorkDetails from "./WorkDetails";
 import { ADD_REFERENCES } from "../../../hooks/ADD_REFERENCES";
 import { ADD_CITATIONS } from "../../../hooks/ADD_CITATIONS";
+import { UPDATE_WORK_NODE } from "../../../hooks/UPDATE_WORK_NODE";
 
 interface WorkNode {
   id: number;
@@ -18,8 +19,34 @@ interface WorkNode {
 function WorkNode({ data }) {
   const [isExpanded, setIsExpanded] = useState(false);
   const ref = useRef(null);
+  const noderef = useRef(null);
   const [contentHeight, setContentHeight] = useState(0);
+  const [xCoordinate, setXCoordinate] = useState(data.workData.xCoordinate);
+  const [yCoordinate, setYCoordinate] = useState(data.workData.yCoordinate);
   const { investigationId } = useParams();
+
+  const [updateNode] = useMutation(UPDATE_WORK_NODE, {
+    variables: {
+      workNodeId: data.workData.id.toString(),
+      xCoordinate: xCoordinate,
+      yCoordinate: yCoordinate,
+      visible: true,
+    },
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let translateMatrix =
+        noderef.current.parentElement.style.transform.split(", ");
+      let x = Number(translateMatrix[0].split("(")[1].split("px")[0]);
+      let y = Number(translateMatrix[1].split("px")[0]);
+      setXCoordinate(x);
+      setYCoordinate(y);
+      updateNode();
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const toggleIsExpanded = useCallback(() => {
     setIsExpanded((isExpanded) => !isExpanded);
@@ -55,6 +82,7 @@ function WorkNode({ data }) {
           ? "#FBD87F"
           : "#B5F8FE",
       }}
+      ref={noderef}
     >
       <Handle type="source" position={Position.Top} id="top-handle" />
       <Handle type="target" position={Position.Bottom} id="bottom-handle" />
