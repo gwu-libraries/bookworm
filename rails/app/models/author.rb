d02117068951401e61aea86a7303bc4b13d153dc
Alex Boyd <alexanderleeboyd@gmail.com>
2024-12-05 16:39:34 +0000

    update author model with author_openalex_id

--- rails/app/models/author.rb
+++ rails/app/models/author.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class Author < ApplicationRecord
-  has_one :authors_ids
-  has_many :authors_counts_by_year
+  has_one :authors_ids,
+          primary_key: :author_openalex_id,
+          foreign_key: :author_openalex_id
+  has_many :authors_counts_by_year,
+           primary_key: :author_openalex_id,
+           foreign_key: :author_openalex_id
   has_many :works_authorships,
            primary_key: :author_openalex_id,
            foreign_key: :author_openalex_id
   has_many :institutions, through: :works_authorships
+  has_many :works, through: :works_authorships
 end
