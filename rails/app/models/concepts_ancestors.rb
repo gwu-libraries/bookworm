a15e5594976ac5a549ffa3a78ab403f06ec9aff8
Alex Boyd <alexanderleeboyd@gmail.com>
2024-12-19 17:19:05 +0000

    remove concepts models

--- rails/app/models/concepts_ancestors.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class ConceptsAncestors < ApplicationRecord
-  belongs_to :concept, foreign_key: 'concept_id', class_name: 'Concept'
-  belongs_to :ancestor, foreign_key: 'ancestor_id', class_name: 'Concept'
-end
--- rails/app/models/concepts_ids.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class ConceptsIds < ApplicationRecord
-  belongs_to :concept
-end
