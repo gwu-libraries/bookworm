8498d5c0c6f3c8de8470fa9ad7db64013a16b065
Alex Boyd <alexanderleeboyd@gmail.com>
2024-12-19 17:28:06 +0000

    Remove works_concept and works_mesh models

--- rails/app/models/works_concept.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class WorksConcept < ApplicationRecord
-  belongs_to :work
-  belongs_to :concept
-end
--- rails/app/models/works_mesh.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class WorksMesh < ApplicationRecord
-  belongs_to :work
-end
