ad47e59d5153414f73126b7ff6b4c5f420752145
Alex Boyd <alexanderleeboyd@gmail.com>
2025-03-04 13:59:13 -0500

    Add orcid url portion to author factory

--- rails/spec/factories/authors.rb
+++ rails/spec/factories/authors.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :author do
-    sequence(:orcid) { |n| "0000-0000-0000-#{n.to_s.rjust(4, "0")}"}
+    sequence(:orcid) { |n| "https://orcid.org/0000-0000-0000-#{n.to_s.rjust(4, "0")}"}
     sequence(:author_openalex_id) { |n| "A#{n.to_s.rjust(10, "0")}"}
     cited_by_count { rand(1..20) }
     works_count { rand(1..20) }
