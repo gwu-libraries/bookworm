808090458b515977715bb0e756153b6a6914c818
Alex Boyd <alexanderleeboyd@gmail.com>
2025-03-04 14:01:34 -0500

    Remove unused services

--- rails/app/services/base_service.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class BaseService
-  def self.openalex_connection
-    Faraday.new(url: 'https://api.openalex.org') do |faraday|
-      faraday.headers['mailto'] = ENV['openalex_mailto']
-    end
-  end
-end
